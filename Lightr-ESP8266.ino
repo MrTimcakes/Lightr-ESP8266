3ee09d28f79681b09a5912986fdfb8f4f1841d16
Timothy Adamson <MrTimcakes@gmail.com>
2017-07-17 22:43:03 +0100

    Config Loading

--- Lightr-ESP8266.ino
+++ Lightr-ESP8266.ino
@@ -1,5 +1,5 @@
 #define DEBUG
-#define FORMAT
+//#define FORMAT
 
 #ifdef DEBUG
  #define DEBUG_PRINTLN(x) Serial.println(x)
@@ -16,30 +16,126 @@
 #include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <ArduinoJson.h>          // JSON Libary https://github.com/bblanchon/ArduinoJson
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  WiFiManager wifiManager;
+char mqtt_server[32];
+char mqtt_port[6] = "1883";
+char mqtt_username[32];
+char mqtt_password[32];
+char lightr_nickname[32];
 
-  #ifdef FORMAT
-    SPIFFS.format();
-    wifiManager.resetSettings();
-    Serial.println("FORMAT COMPLETE");
+void setup() {
+  #ifdef DEBUG
+    Serial.begin(115200);
   #endif
   
-  wifiManager.autoConnect( String( "Lightr " + WiFi.macAddress() ).c_str() );
-  
-  DEBUG_PRINTLN("Test");
-  DEBUG_PRINTLN("Lightr " + WiFi.macAddress());
-  //DEBUG_PRINTLN(wifiManager.ConfigPortalSSID());
+  startWiFi(); //Start WiFi + Load config, will not progress past until connected
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  DEBUG_PRINTLN();
+  DEBUG_PRINTLN(WiFi.localIP());
+  DEBUG_PRINTLN(lightr_nickname);
+  DEBUG_PRINTLN(mqtt_server);
+  DEBUG_PRINTLN(mqtt_port);
+  DEBUG_PRINTLN(mqtt_username);
+  DEBUG_PRINTLN(mqtt_password);
 }
 
 void loop() {
-  DEBUG_PRINTLN("Test");
-  Serial.println("Test2");
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  delay(500);
+  // Loop Stuff
+}
+
+void startWiFi(){
+  strcpy(lightr_nickname, String( "Lightr " + WiFi.macAddress() ).c_str()); // Hack to get nickname to default to "Lightr xx:xx:xx:xx:xx:xx"
+  
+  #ifdef FORMAT
+    WiFiManager wifiMgr;
+    SPIFFS.format();
+    wifiMgr.resetSettings();
+    Serial.println("FORMAT COMPLETE");
+  #endif
+  
+  loadConfig(); 
+  
+  WiFiManager wifiManager;
+  
+  WiFiManagerParameter custom_mqtt_server("server", "MQTT Server", mqtt_server, 32);
+  WiFiManagerParameter custom_mqtt_port("port", "MQTT Port", mqtt_port, 6);
+  WiFiManagerParameter custom_mqtt_username("username", "MQTT Username", mqtt_username, 32);
+  WiFiManagerParameter custom_mqtt_password("password", "MQTT Password", mqtt_password, 32);
+  WiFiManagerParameter custom_nickname("nickname", "Light Nickname", lightr_nickname, 32); 
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_username);
+  wifiManager.addParameter(&custom_mqtt_password);
+  wifiManager.addParameter(&custom_nickname);
+  
+  wifiManager.setSaveConfigCallback(saveConfig);
+  wifiManager.setAPStaticIPConfig(IPAddress(192,168,0,1), IPAddress(192,168,0,1), IPAddress(255,255,255,0));
+  
+  
+  wifiManager.autoConnect(lightr_nickname);
+  DEBUG_PRINTLN("Successfully Connected!");
+  
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_username, custom_mqtt_username.getValue());
+  strcpy(mqtt_password, custom_mqtt_password.getValue());
+  strcpy(lightr_nickname, custom_nickname.getValue());
+}
+
+void loadConfig(){
+  DEBUG_PRINTLN("mounting FS...");
+  if (SPIFFS.begin()) {
+    DEBUG_PRINTLN("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      DEBUG_PRINTLN("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        DEBUG_PRINTLN("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        //json.printTo(Serial);
+        if (json.success()) {
+          DEBUG_PRINTLN("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(mqtt_username, json["mqtt_username"]);
+          strcpy(mqtt_password, json["mqtt_password"]);
+          strcpy(lightr_nickname, json["nickname"]);
+
+        } else {
+          DEBUG_PRINTLN("failed to load json config");
+        }
+      }
+    }
+  } else {
+    DEBUG_PRINTLN("failed to mount FS");
+  }
+}
+
+void saveConfig(){
+  DEBUG_PRINTLN("saving config");
+  
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  
+  json["mqtt_server"] = mqtt_server;
+  json["mqtt_port"] = mqtt_port;
+  json["mqtt_username"] = mqtt_username;
+  json["mqtt_password"] = mqtt_password;
+  json["nickname"] = lightr_nickname;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile) {
+    DEBUG_PRINTLN("failed to open config file for writing");
+  }
+  #ifdef DEBUG
+    json.printTo(Serial);
+  #endif
+  json.printTo(configFile);
+  configFile.close();
 }
